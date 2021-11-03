4e072f494bfe8ffb1c5c3ba45f6e1af01e4c08b5
Ebube Ud <kripsonud@gmail.com>
2021-11-03 09:05:34 +0100

    Remove Account Abis

--- src/abi/core/index.ts
+++ src/abi/core/index.ts
@@ -1,18 +1,12 @@
-export * as accountV2_M1 from './v2/accountM1';
 export * as connectorsV2_M1 from './v2/connectorsM1';
-export * as accountProxy from './v2/accountProxy';
 export * as implementations from './v2/implementations';
-export * as accountDefault from './v2/accountDefault';
 
 export * as index from './indexItem';
 export * as list from './list';
 export * as read from './read';
 
-import { accountM1 as accountV2_M1 } from './v2/accountM1';
 import * as connectorsV2_M1 from './v2/connectorsM1';
-import * as accountProxy from './v2/accountProxy';
 import * as implementations from './v2/implementations';
-import * as accountDefault from './v2/accountDefault';
 
 import { index } from './indexItem';
 import { list } from './list';
@@ -24,11 +18,8 @@ export const core = {
   read,
   versions: {
     2: {
-      accountProxy,
-      accountDefault,
       // connectorsProxy,
       implementations,
-      account: accountV2_M1,
       connectors: connectorsV2_M1,
     },
   },
