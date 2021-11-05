9ab142929da0fd7818256364cd772bb209d954cf
Ebube Ud <kripsonud@gmail.com>
2021-11-05 20:55:31 +0100

    1.0.5

--- src/nub.ts
+++ src/nub.ts
@@ -178,19 +178,15 @@ export class NUB {
     return transaction;
   }
 
-  public async transferToken(tokenAddress: string, receiver: string, amount: number){
-    const contract = new this.web3.eth.Contract(
-      Abi.basics.erc20,
-      tokenAddress,
-    );
+  public async transferToken(tokenAddress: string, receiver: string, amount: number) {
+    const contract = new this.web3.eth.Contract(Abi.basics.erc20, tokenAddress);
     const resp = await contract.methods.transfer(receiver, amount).send();
     return resp;
   }
 
-  public async transferEth(receiver: string, amount: number)
-  {
+  public async transferEth(receiver: string, amount: number) {
     const sender = await this.internal.getAddress();
-    const resp = await this.web3.eth.sendTransaction({ from:sender,to:receiver, value:amount });
+    const resp = await this.web3.eth.sendTransaction({ from: sender, to: receiver, value: amount });
     return resp;
   }
 
