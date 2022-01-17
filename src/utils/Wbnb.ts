a7f431aada558af986f9c8addd65bb1415d14dcc
nonseodion <nonseifebhor@gmail.com>
2022-01-17 15:53:19 +0100

    added wrapping and unwrapping BNB

---
+++ src/utils/Wbnb.ts
@@ -0,0 +1,116 @@
+import NUB from "..";
+import { Abi } from "../constants/abi";
+import { Addresses } from "../constants/addresses";
+import { TransactionConfig } from 'web3-core';
+import { GetTransactionConfigParams } from "../internal";
+
+
+type WbnbParams = {
+  amount: number|string 
+  } & Pick<TransactionConfig, 'from' | 'gas' | 'gasPrice' | 'nonce' >;
+
+export default class Wbnb {
+    contractInstance;
+    nubInstance: NUB;
+    address: string;
+    constructor(nub: NUB)
+    {
+      this.contractInstance = new nub.web3.eth.Contract(Abi.Wbnb, Addresses.tokens.chains[nub.CHAIN_ID].WBNB);
+      this.nubInstance = nub;
+      this.address = Addresses.tokens.chains[this.nubInstance.CHAIN_ID].WBNB;
+    }
+
+    async estimateWrapGas(params: WbnbParams){
+      const txObj = await this.wrapTxObj(params);
+      const gas = await this.getGas(txObj);
+      return {
+        gas,
+        price: this.nubInstance.GAS_PRICE,
+        fee: +gas*this.nubInstance.GAS_PRICE
+      }
+    }
+
+    async wrap(params: WbnbParams){
+      const txObj = await this.wrapTxObj(params);
+      const resp = await this.nubInstance.sendTransaction(txObj);
+      return resp;
+    }
+
+    async unwrap(params: WbnbParams){
+      const txObj = await this.unwrapTxObj(params);
+      console.log(txObj);
+      const resp = await this.nubInstance.sendTransaction(txObj);
+      console.log("ewif");
+      return resp;
+    }
+
+    private async wrapTxObj(params: WbnbParams): Promise<TransactionConfig> {
+
+      if (!params.amount) {
+        throw new Error("'amount' is not a number");
+      }
+
+      if (!params.from) {
+        params.from = await this.nubInstance.internal.getAddress();
+      }
+
+      let txObj: TransactionConfig;
+
+      if (['-1', this.nubInstance.maxValue].includes(params.amount)) {
+        throw new Error("BNB amount value cannot be passed as '-1'.");
+      }
+
+      txObj = await this.nubInstance.internal.getTransactionConfig({
+        from: params.from,
+        to: this.address,
+        data: this.contractInstance.methods.deposit().encodeABI(),
+        gas: params.gas,
+        gasPrice: params.gasPrice,
+        nonce: params.nonce,
+        value: params.amount,
+      } as GetTransactionConfigParams);
+
+      return txObj;
+    }
+
+    private async unwrapTxObj(params: WbnbParams): Promise<TransactionConfig>{
+      if (!params.amount) {
+        throw new Error("'amount' is not a number");
+      }
+
+      if (!params.from) {
+        params.from = await this.nubInstance.internal.getAddress();
+      }
+
+      if (['-1', this.nubInstance.maxValue].includes(params.amount)) {
+        params.amount = await this.contractInstance.methods.balanceOf(params.from)
+      }
+
+      let txObj: TransactionConfig;
+      txObj = await this.nubInstance.internal.getTransactionConfig({
+        from: params.from,
+        to: this.address,
+        data: this.contractInstance.methods.withdraw(params.amount).encodeABI(),
+        gas: params.gas,
+        gasPrice: params.gasPrice,
+        nonce: params.nonce,
+        value: 0,
+      } as GetTransactionConfigParams);
+
+      return txObj;
+    }
+
+    async estimateUnwrapGas(params: WbnbParams){
+      const txObj = await this.unwrapTxObj(params);
+      const gas = await this.getGas(txObj);
+      return {
+        gas,
+        price: this.nubInstance.GAS_PRICE,
+        fee: +gas*this.nubInstance.GAS_PRICE
+      }
+    }
+
+    private getGas = async (transactionConfig: TransactionConfig): Promise<string> => {
+      return ((await this.nubInstance.web3.eth.estimateGas(transactionConfig)) * 1.1).toFixed(0); // increasing gas cost by 10% for margin: ;
+    };
+  }
